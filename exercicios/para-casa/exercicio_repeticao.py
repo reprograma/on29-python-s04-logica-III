863b2f68d924592d08920d736d87a0ef815015fe
Lucy <souzalucyg@gmail.com>
2024-04-22 10:00:59 -0300

    adicionando if

--- exercicios/para-casa/exercicio_repeticao.py
+++ exercicios/para-casa/exercicio_repeticao.py
@@ -2,5 +2,8 @@ tabuada = int(input("Digite o número da tabuada: "))
 comeca = int(input("Digite o número de início da tabuada: "))
 fim = int(input("Digite o número de fim da tabuada: "))
 
-for numero in range(comeca, fim, 1):
-    print(f"tabuada do {tabuada}, {numero} * {tabuada} = {numero*tabuada}")
+if comeca > fim:
+    print("A variável de começo não pode ser maior que a de fim.")
+else:
+    for numero in range(comeca, fim, 1):
+        print(f"tabuada do {tabuada}, {numero} * {tabuada} = {numero*tabuada}")
